f15c31d8631922474a9f16fb2be5ac9658bd0737
Otávio-Figueiredo <otaviovinicius3000@gmail.com>
2025-10-24 00:20:21 -0300

    Update Programa.java

--- Agregação e composição/Classe Professor/Programa.java
+++ Agregação e composição/Classe Professor/Programa.java
@@ -1,1 +1,15 @@
+package ClasseProfessor;
 
+public class Programa {
+    public static void main(String[] args) {
+        Professor prof1 = new Professor("Dr. Deyvid");
+        Turma turma = new Turma("POO101");
+
+        turma.setProfessor(prof1);
+        turma.resumo();
+
+        Professor prof2 = new Professor("Profa. Samara");
+        turma.setProfessor(prof2);
+        turma.resumo();
+    }
+}
